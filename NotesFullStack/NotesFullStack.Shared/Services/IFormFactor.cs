f30cf610eabef45233fc505d060579b5771239e5
Jennifer DeCota <jenniferd@zo-organized.com>
2025-08-06 14:58:09 -0500

    files added

---
+++ NotesFullStack/NotesFullStack.Shared/Services/IFormFactor.cs
@@ -0,0 +1,8 @@
+namespace NotesFullStack.Shared.Services
+{
+    public interface IFormFactor
+    {
+        public string GetFormFactor();
+        public string GetPlatform();
+    }
+}
---
+++ NotesFullStack/NotesFullStack.Web/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using NotesFullStack.Shared.Services;
+
+namespace NotesFullStack.Web.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "Web";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
